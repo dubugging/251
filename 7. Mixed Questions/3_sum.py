0378f0665c5a1253deb4daaa671c86f2633237b1
dubugging <kirtanarunava145@gmail.com>
2023-12-04 12:51:45 +0530

    done with 3 sum

--- 7. Mixed Questions/3_sum.py
+++ 7. Mixed Questions/3_sum.py
@@ -1,32 +1,52 @@
-def brute(n: int, arr: [int]) -> [[int]]:
+def brute(arr, n, target) -> [[int]]:
     ans = set()
     for i in range(n):
         for j in range(i+1, n):
             for k in range(j+1, n):
-                if not arr[i] + arr[j] + arr[k]:
+                if arr[i] + arr[j] + arr[k] == target:
                     temp = [arr[i], arr[j], arr[k]]
                     temp.sort()
                     ans.add(tuple(temp))
     return list(ans)
 
 
-def better(n: int, arr: [int]) -> [[int]]:
+def better(arr, n, target) -> [[int]]:
     ans = set()
     for i in range(n):
         st = set()
         for j in range(i+1, n):
-            find = -(arr[i]+arr[j])
+            find = target - (arr[i]+arr[j])
             if find not in st:
                 st.add(arr[j])
             elif find in st:
-                temp = [arr[i], find, arr[j]]
+                temp = [arr[i], arr[j], find]
                 temp.sort()
                 ans.add(tuple(temp))
     return [list(item) for item in ans]
 
 
-def optimal(n: int, arr: [int]) -> [[int]]:
-    pass
+def optimal(arr, n, target) -> [[int]]:
+    arr.sort()
+    ans = []
+    for i in range(n):
+        if i > 0 and arr[i-1] == arr[i]:
+            continue
+        j, k = i+1, n-1
+        while j < k:
+            total = arr[i] + arr[j] + arr[k]
+            if total == target:
+                ans.append([arr[i], arr[j], arr[k]])
+                j += 1
+                k -= 1
+                while j < k and arr[j] == arr[j-1]:
+                    j += 1
+                while j < k and arr[k] == arr[k+1]:
+                    k -= 1
+            elif total < k:
+                j += 1
+            else:
+                k -= 1
+    return ans
 
 
-print(better(n=5, arr=[-1, -1, 2, 0, 1]))
+print(optimal(n=5, arr=[-1, -1, 2, 0, 1], target=2))
