f07936c08af880c891ae9da8b605597c2aece112
dubugging <kirtanarunava145@gmail.com>
2024-01-04 08:22:29 +0530

    done with swapping node

--- 10. Linked List/swap_nodes_in_pairs.py
+++ 10. Linked List/swap_nodes_in_pairs.py
@@ -1,2 +1,8 @@
 def pairsSwap(head):
-    pass
+    if not head or not head.next:
+        return head
+    new_head = head.next
+    third = head.next.next
+    new_head.next = head
+    head.next = pairsSwap(third)
+    return new_head
