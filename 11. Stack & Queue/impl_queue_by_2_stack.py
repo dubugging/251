f04f2e0d2142bb799f76f82fc676b7290f7b068a
dubugging <kirtanarunava145@gmail.com>
2024-01-10 08:08:28 +0530

    None

---
+++ 11. Stack & Queue/impl_queue_by_2_stack.py
@@ -0,0 +1,19 @@
+class Queue:
+    # Stacks to be used in the operations.
+    def __init__(self):
+        self.stk1 = []
+        self.stk2 = []
+
+    # Enqueues 'X' into the queue. Returns true after enqueuing.
+    def enqueue(self, X):
+        # Write your code here
+        pass
+
+    """
+      Dequeues top element from queue. Returns -1 if the queue is empty, 
+      otherwise returns the popped element.
+    """
+
+    def dequeue(self):
+        # Write your code here
+        pass
---
+++ 11. Stack & Queue/impl_stack_by_2_queues.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        # Define the data members.
+        pass
+
+    def getSize(self) -> int:
+        # Implement the getSize() function.
+        pass
+
+    def isEmpty(self) -> bool:
+        # Implement the isEmpty() function.
+        pass
+
+    def push(self, element: int) -> None:
+        # Implement the push() function.
+        pass
+
+    def pop(self) -> int:
+        # Implement the pop() function.
+        pass
+
+    def top(self) -> int:
+        # Implement the top() function.
+        pass
