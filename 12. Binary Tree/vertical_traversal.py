488a0d3d7af81bb7e175a9b11a4e72e492c0fa8e
Arunava Kirtan <kirtanarunava145@gmail.com>
2024-02-22 15:36:51 +0530

    "all"

--- 12. Binary Tree/vertical_traversal.py
+++ 12. Binary Tree/vertical_traversal.py
@@ -1,4 +1,16 @@
-def verticalOrderTraversal(root):
-    # Write your code here
-    # Return a list
-    pass
+# from collections import deque
+
+
+# def verticalOrderTraversal(root):
+#     nodes = {}
+#     q = deque()
+#     ans = []
+
+#     if not root:
+#         return ans
+
+#     q.append(make_pair(root, make_pair(0, 0)))
+
+#     while q:
+#         temp = q.popleft()
+#         hd =
